12803adb0f7240a6f77f5c3bed525082081b4790
Rajat <rajat@eleven01.io>
2019-10-07 13:39:24 +0530

    add responseHandler in API

---
+++ lib/responseHandler/response.go
@@ -0,0 +1,17 @@
+package response
+
+import (
+    "net/http"
+    "encoding/json"
+    "gopkg.in/mgo.v2/bson"
+)
+
+
+func Json(w http.ResponseWriter, code int, msg string, payload interface{}) {
+    w.WriteHeader(code)
+    if payload == false {
+        json.NewEncoder(w).Encode(bson.M{"code": code, "success": false, "msg": msg, "data": nil})
+        return
+    }
+    json.NewEncoder(w).Encode(bson.M{"code": code, "success": true, "msg": msg, "data": payload})
+}
